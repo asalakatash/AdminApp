553d440f8f6210ea1d414ced2cfb9ef437b47305
Malik Abed Alhade <malikabedelhade@gmail.com>
2019-01-16 16:40:07 +0200

    First commit

---
+++ src/main/java/com/Admin/AdminApp/AdminAppApplication.java
@@ -0,0 +1,14 @@
+package com.Admin.AdminApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminAppApplication.class, args);
+	}
+
+}
+
